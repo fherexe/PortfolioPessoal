c5a7715b75a5237b99f2d3021ec66ed82944c69f
Fernando Souza <114751334+fherexe@users.noreply.github.com>
2023-05-28 17:09:54 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+//--------------------------Hamburger Menu--------------------------
+
+const menuHamburger = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('.menu');
+
+menuHamburger.addEventListener('click', () =>{
+  menu.classList.toggle('active');
+  menuHamburger.classList.toggle('active');
+  
+  //toggle vai definir que quando clicar vai fazer a ação e quando clicar novamente vai desfazer
+} ); //atribuir ação a uma função 
+
+
+//-------------------- Marque ---------------------
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
