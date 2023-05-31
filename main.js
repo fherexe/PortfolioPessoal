48df2f14880f4e6a38df8e51990985df53d50c44
Fernando Souza <114751334+fherexe@users.noreply.github.com>
2023-05-31 01:51:44 -0300

    att

--- main.js
+++ main.js
@@ -21,3 +21,21 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for(let i=0; i<marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
+
+
+//-------------------- Botão voltar ao topo  ---------------------
+
+window.onscroll = function() {scrollFunction()};
+    
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("voltarTopo").style.display = "block";
+  } else {
+    document.getElementById("voltarTopo").style.display = "none";
+  }
+}
+
+function voltarAoTopo() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
